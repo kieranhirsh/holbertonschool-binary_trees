59ffc6619ddae7185985af117fd88fcdd51b04e9
kieranhirsh <kieranhirsh@gmail.com>
2024-04-11 13:32:05 +0000

    C - Binary Trees. Task 3. Delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes a binary tree
+ * @tree: a pointer to the root node
+ *
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree->left != NULL)
+		binary_tree_delete(tree->left);
+
+	if (tree->right != NULL)
+		binary_tree_delete(tree->right);
+
+	free(tree);
+}
